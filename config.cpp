75fc0411b55ba8b2456e9c41451d07ba04eb6d95
ZluskeN <pontux@gmail.com>
2017-08-14 17:43:40 +0200

    Tweaked chemlight shield
    CTRG and Viper Team no longer get them and the protagonist does

--- config.cpp
+++ config.cpp
@@ -2608,6 +2608,8 @@ class CfgVehicles
 		respawnweapons[] = {"ZSN_arifle_SPAR_01_GL_khk_Hamr_pointer_F","hgun_P07_snds_F","Throw","Put"};
 		magazines[] = {"30Rnd_556x45_Stanag","30Rnd_556x45_Stanag","30Rnd_556x45_Stanag","30Rnd_556x45_Stanag","30Rnd_556x45_Stanag","30Rnd_556x45_Stanag","16Rnd_9x21_Mag","16Rnd_9x21_Mag","HandGrenade","HandGrenade","Chemlight_green","1Rnd_HE_Grenade_shell","1Rnd_HE_Grenade_shell","1Rnd_HE_Grenade_shell","1Rnd_Smoke_Grenade_shell","1Rnd_SmokeGreen_Grenade_shell"};
 		respawnmagazines[] = {"30Rnd_556x45_Stanag","30Rnd_556x45_Stanag","30Rnd_556x45_Stanag","30Rnd_556x45_Stanag","30Rnd_556x45_Stanag","30Rnd_556x45_Stanag","16Rnd_9x21_Mag","16Rnd_9x21_Mag","HandGrenade","HandGrenade","Chemlight_green","1Rnd_HE_Grenade_shell","1Rnd_HE_Grenade_shell","1Rnd_HE_Grenade_shell","1Rnd_Smoke_Grenade_shell","1Rnd_SmokeGreen_Grenade_shell"};
+		Items[] = {"FirstAidKit","ACE_Chemlight_Shield"};
+		RespawnItems[] = {"FirstAidKit","ACE_Chemlight_Shield"};
 	};
 	class B_CTRG_soldier_AR_A_F: B_Soldier_base_F
 	{
@@ -2619,8 +2621,6 @@ class CfgVehicles
 		respawnWeapons[] = {"ZSN_arifle_MX_Black_ERCO_pointer_F","hgun_P07_khk_Snds_F","Throw","Put","Rangefinder"};
 		magazines[] = {"30Rnd_65x39_caseless_mag_Tracer","30Rnd_65x39_caseless_mag_Tracer","30Rnd_65x39_caseless_mag","30Rnd_65x39_caseless_mag","30Rnd_65x39_caseless_mag","30Rnd_65x39_caseless_mag","16Rnd_9x21_Mag","16Rnd_9x21_Mag","16Rnd_9x21_Mag","MiniGrenade","MiniGrenade","SmokeShell","SmokeShellGreen","SmokeShellBlue","SmokeShellOrange","Chemlight_green","Chemlight_green"};
 		respawnMagazines[] = {"30Rnd_65x39_caseless_mag_Tracer","30Rnd_65x39_caseless_mag_Tracer","30Rnd_65x39_caseless_mag","30Rnd_65x39_caseless_mag","30Rnd_65x39_caseless_mag","30Rnd_65x39_caseless_mag","16Rnd_9x21_Mag","16Rnd_9x21_Mag","16Rnd_9x21_Mag","MiniGrenade","MiniGrenade","SmokeShell","SmokeShellGreen","SmokeShellBlue","SmokeShellOrange","Chemlight_green","Chemlight_green"};
-		Items[] = {"FirstAidKit","ACE_Chemlight_Shield"};
-		RespawnItems[] = {"FirstAidKit","ACE_Chemlight_Shield"};
 	};
 	class B_CTRG_Soldier_F;
 	class B_CTRG_Soldier_TL_tna_F : B_CTRG_Soldier_F
@@ -3209,6 +3209,7 @@ class CfgVehicles
 		Items[] = {"FirstAidKit","ACE_Chemlight_Shield"};
 		RespawnItems[] = {"FirstAidKit","ACE_Chemlight_Shield"};
 	};
+	class I_G_Soldier_F: I_G_Soldier_base_F{};
 	class I_G_Soldier_AR_F: I_G_Soldier_base_F
 	{
 		weapons[] = {"LMG_Zafir_F","Throw","Put"};
@@ -3222,6 +3223,12 @@ class CfgVehicles
 		respawnWeapons[] = {"ZSN_srifle_DMR_06_olive_MRCO_F","Throw","Put","Binocular"};
 		magazines[] = {"20Rnd_762x51_Mag","20Rnd_762x51_Mag","20Rnd_762x51_Mag","20Rnd_762x51_Mag","20Rnd_762x51_Mag","20Rnd_762x51_Mag","20Rnd_762x51_Mag","20Rnd_762x51_Mag","HandGrenade","MiniGrenade","SmokeShell","SmokeShellGreen","Chemlight_blue","Chemlight_blue"};
 		respawnMagazines[] = {"20Rnd_762x51_Mag","20Rnd_762x51_Mag","20Rnd_762x51_Mag","20Rnd_762x51_Mag","20Rnd_762x51_Mag","20Rnd_762x51_Mag","20Rnd_762x51_Mag","20Rnd_762x51_Mag","HandGrenade","MiniGrenade","SmokeShell","SmokeShellGreen","Chemlight_blue","Chemlight_blue"};
+	};
+	class B_G_Soldier_F : I_G_Soldier_F{};
+	class I_G_Story_Protagonist_F : B_G_Soldier_F
+	{
+		Items[] = {"FirstAidKit","ACE_Chemlight_Shield"};
+		RespawnItems[] = {"FirstAidKit","ACE_Chemlight_Shield"};
 	};
 	class I_C_Soldier_base_F : I_G_Soldier_base_F{};
 	class I_C_Soldier_Para_4_F : I_C_Soldier_base_F
@@ -3722,11 +3729,6 @@ class CfgVehicles
 		respawnWeapons[] = {"ZSN_MMG_01_tan_ARCO_LP_F","hgun_Pistol_heavy_02_Yorris_F","Throw","Put"};
 		magazines[] = {"150Rnd_93x64_Mag","150Rnd_93x64_Mag","150Rnd_93x64_Mag","6Rnd_45ACP_Cylinder","6Rnd_45ACP_Cylinder","6Rnd_45ACP_Cylinder","SmokeShell","SmokeShellRed","Chemlight_red","Chemlight_red"};
 		respawnMagazines[] = {"150Rnd_93x64_Mag","150Rnd_93x64_Mag","150Rnd_93x64_Mag","6Rnd_45ACP_Cylinder","6Rnd_45ACP_Cylinder","6Rnd_45ACP_Cylinder","SmokeShell","SmokeShellRed","Chemlight_red","Chemlight_red"};
-	};
-	class O_V_Soldier_base_F : O_Soldier_base_F
-	{		
-		Items[] = {"FirstAidKit","ACE_Chemlight_Shield"};
-		RespawnItems[] = {"FirstAidKit","ACE_Chemlight_Shield"};
 	};
 	class Car_F;
 	class MRAP_01_base_F : Car_F
