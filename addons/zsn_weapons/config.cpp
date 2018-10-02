4ea8e82cf30d29e4350db55aebcd6ea220f62cc0
ZluskeN <pontux@gmail.com>
2018-10-02 20:52:10 +0200

    Changed sniper weapons

--- addons/zsn_weapons/config.cpp
+++ addons/zsn_weapons/config.cpp
@@ -2859,8 +2859,8 @@ class CfgVehicles
 		weapons[] = {"ZSN_arifle_SPAR_01_GL_snd_Holo_pointer_snds_F","hgun_P07_khk_Snds_F","Throw","Put","Laserdesignator"};
 		respawnWeapons[] = {"ZSN_arifle_SPAR_01_GL_snd_Holo_pointer_snds_F","hgun_P07_khk_Snds_F","Throw","Put","Laserdesignator"};
 	};
-	class B_Soldier_sniper_base_F: B_Soldier_base_F{};
-	class B_ghillie_base_F : B_Soldier_sniper_base_F
+	class B_ghillie_base_F;
+	class B_ghillie_lsh_F : B_ghillie_base_F
 	{
 		weapons[] = {"srifle_LRR_tna_LRPS_F","hgun_P07_snds_F","Throw","Put","Rangefinder"};
 		respawnWeapons[] = {"srifle_LRR_tna_LRPS_F","hgun_P07_snds_F","Throw","Put","Rangefinder"};
@@ -2869,11 +2869,6 @@ class CfgVehicles
 	{
 		weapons[] = {"srifle_LRR_tna_LRPS_F","hgun_P07_khk_Snds_F","Throw","Put","Rangefinder"};
 		respawnWeapons[] = {"srifle_LRR_tna_LRPS_F","hgun_P07_khk_Snds_F","Throw","Put","Rangefinder"};
-	};
-	class B_sniper_F : B_Soldier_sniper_base_F
-	{
-		weapons[] = {"srifle_LRR_tna_LRPS_F","hgun_P07_snds_F","Throw","Put","Rangefinder"};
-		respawnWeapons[] = {"srifle_LRR_tna_LRPS_F","hgun_P07_snds_F","Throw","Put","Rangefinder"};
 	};
 	class B_T_sniper_F : B_sniper_F
 	{
